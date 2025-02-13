8c016018249080912b47e0378bf73e925be98e01
tdtonmoy <112154526+tdtonmoy@users.noreply.github.com>
2025-02-13 19:18:29 +0530

    gg

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("openPopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "flex";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
